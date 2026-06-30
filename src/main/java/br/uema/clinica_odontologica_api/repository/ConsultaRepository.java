e352db23ca392bc96ed0c007e49f145f72dbc480
felipefreitas21 <antoniofelipefreitas23@gmail.com>
2026-06-30 01:24:30 -0300

    Relatório das Consultas

--- src/main/java/br/uema/clinica_odontologica_api/repository/ConsultaRepository.java
+++ src/main/java/br/uema/clinica_odontologica_api/repository/ConsultaRepository.java
@@ -2,8 +2,25 @@ package br.uema.clinica_odontologica_api.repository;
 
 import br.uema.clinica_odontologica_api.entity.Consulta;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ConsultaRepository extends JpaRepository<Consulta, Integer> {
+    @Query(value = """
+        SELECT p.nome AS paciente,
+               d.nome AS dentista,
+               e.nome_especialidade AS especialidade,
+               c.data_consulta,
+               c.hora_consulta,
+               c.status_consulta
+        FROM consulta c
+        JOIN paciente p ON c.id_paciente = p.id_paciente
+        JOIN dentista d ON c.id_dentista = d.id_dentista
+        JOIN especialidade e ON d.id_especialidade = e.id_especialidade
+        ORDER BY c.data_consulta DESC
+    """, nativeQuery = true)
+    List<Object[]> historicoConsultas();
 }
